afbd9e5b961d8dcfe458f42f846a8aa9242395e8
Langjan <35696770+LangjanDu@users.noreply.github.com>
2018-08-30 23:31:09 +0800

    增加多线程

--- Pic_Spider_zhuoku.com.py
+++ Pic_Spider_zhuoku.com.py
@@ -1,7 +1,8 @@
+from time import sleep, ctime 
+import threading
 import requests
 import re
 import os
-import time
 
 folder_path = 'F:\PyDowns\zhuoku'
 os.chdir(folder_path)
@@ -21,33 +22,46 @@ def GetCarsList(url):
     return CarsList
 
 def DownPics(url):
-    CarsList = GetCarsList(url)
-    for i in CarsList:
-        PicsUrl = []
-        Count = 1
+    PicsUrl = []
+    Count = 1
+    result = GetHtml(url)
+    Title =  re.findall(r'<title>(.*?)\(', result)[0]
+    if not os.path.isdir(Title):
+        os.mkdir(Title)
+    PicNum = re.findall('下一张</a><a href=\d+\((.*?)\)', result)[0]
+    href = re.findall('下一张</a><a href=(.*?)\(', result)[0]
+    for i in range(1, int(PicNum)+1):
+        PicsUrl.append('http://www.zhuoku.com/zhuomianbizhi/jing-car/' + href + '(%d).htm#turn' % i)
+    for i in PicsUrl:
         result = GetHtml(i)
-        Title =  re.findall(r'<title>(.*?)\(', result)[0]
-        if not os.path.isdir(Title):
-            os.mkdir(Title)
-        PicNum = re.findall('下一张</a><a href=\d+\((.*?)\)', result)[0]
-        href = re.findall('下一张</a><a href=(.*?)\(', result)[0]
-        for i in range(1, int(PicNum)+1):
-            PicsUrl.append('http://www.zhuoku.com/zhuomianbizhi/jing-car/' + href + '(%d).htm#turn' % i)
-        for i in PicsUrl:
-            result = GetHtml(i)
-            PicName = re.findall(r'thumbs/tn_(.*?)"', result)[0]
-            Path = folder_path + "\\" + Title + "\\" + PicName
-            PicUrl = re.findall(r'<img id="imageview" src="(.*?)"', result)[0]
-            print (PicUrl)
-            
-            headers = {
-            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-            'Referer':'%s' % i} #破防盗链        
-            with open(Path, 'wb') as f:
-                f.write(requests.get(PicUrl, headers = headers).content)
-                print ("正在下载《%s》的第%d/%d张" % (Title, Count, int(PicNum)))
-                Count += 1
-                time.sleep(1)
+        PicName = re.findall(r'thumbs/tn_(.*?)"', result)[0]
+        Path = folder_path + "\\" + Title + "\\" + PicName
+        PicUrl = re.findall(r'<img id="imageview" src="(.*?)"', result)[0]
+        
+        headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+        'Referer':'%s' % i} #反防盗链        
+        with open(Path, 'wb') as f:
+            f.write(requests.get(PicUrl, headers = headers).content)
+            print ("正在下载《%s》的第%d/%d张" % (Title, Count, int(PicNum)))
+            Count += 1
+            sleep(1.5)
 
-url = 'http://www.zhuoku.com/zhuomianbizhi/jing-car/index-1.htm'
-DownPics(url)
+def main():
+    #获取第一页所有图辑URL
+    url = 'http://www.zhuoku.com/zhuomianbizhi/jing-car/index-1.htm'
+    CarsList = GetCarsList(url)
+    #创建线程，一个图辑一个线程
+    threads = []
+    num = range(len(CarsList))
+    for url in CarsList:
+        t = threading.Thread(target = DownPics, args=(url,))
+        threads.append(t)
+    #启动线程
+    for i in num:
+        threads[i].start()
+    for i in num:
+        threads[i].join()
+
+if __name__ == '__main__':
+    main()
